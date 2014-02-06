ed1ef28775fd3cfe09c83ae25a10d0ca275b28f5
CityTransit <nicorichard@gmail.com>
2014-02-06 13:06:49 -0600

    Socket.io is working

--- OchoPhone/ViewController.h
+++ OchoPhone/ViewController.h
@@ -6,10 +6,11 @@
 //  Copyright (c) 2014 UManitobaOcho. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "SocketIO.h"
 @interface ViewController : UIViewController <SocketIODelegate>
 @property (nonatomic,strong) SocketIO* socketIO;
+
+@property (nonatomic,assign) IBOutlet UITextView* msgLog;
 
 @end
--- OchoPhone/ViewController.m
+++ OchoPhone/ViewController.m
@@ -9,6 +9,48 @@
 #import "SocketIOPacket.h"
 #import "ViewController.h"
 
+@interface ViewController ()
+
+@end
+
 @implementation ViewController
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"loaded the view");
+    _socketIO = [[SocketIO alloc] initWithDelegate:self];
+    [_socketIO connectToHost:@"localhost" onPort:3000];
+    [_socketIO sendEvent:@"connect" withData:@"iOSuser"];
+}
+
+- (void) socketIODidConnect:(SocketIO *)socket
+{
+    NSLog(@"socket.io connected.");
+}
+
+- (void) socketIO:(SocketIO *)socket didReceiveEvent:(SocketIOPacket *)packet
+{
+    NSLog(@"didReceiveEvent()");
+    
+    if([packet.name isEqualToString:@"message"])
+    {
+        NSArray* args = packet.args;
+        NSDictionary* arg = args[0];
+        
+        _msgLog.text = [_msgLog.text stringByAppendingFormat:@"%@\n",arg[@"message"]];
+        
+    }
+}
+
+- (void) socketIO:(SocketIO *)socket onError:(NSError *)error
+{
+    NSLog(@"onError() %@", error);
+}
+
+
+- (void) socketIODidDisconnect:(SocketIO *)socket disconnectedWithError:(NSError *)error
+{
+    NSLog(@"socket.io disconnected. did error occur? %@", error);
+}
 
 @end
