d5dc5c01e5cf3442aaba5d1502d9e07e3f0f947f
CityTransit <nicorichard@gmail.com>
2014-02-25 12:46:32 -0600

    socket.io-objc

---
+++ OchoPhone/OchoPhone/ClassCell.h
@@ -0,0 +1,13 @@
+//
+//  ClassCell.h
+//  OchoPhone
+//
+//  Created by Nico on 2/25/2014.
+//  Copyright (c) 2014 UManitobaOcho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClassCell : UITableViewCell
+
+@end
