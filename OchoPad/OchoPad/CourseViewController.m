279d0c3e5716d4399a299f63a5287161ce92407b
Alexander Plishka <umplishk@beaver.cs.umanitoba.ca>
2014-03-04 13:35:19 -0600

    Can delete rows from course view table using swipe (then pressing delete button).

--- OchoPad/OchoPad/CourseViewController.m
+++ OchoPad/OchoPad/CourseViewController.m
@@ -95,28 +95,28 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
+        [self.courses removeObjectAtIndex:[indexPath row]];
+        
+        [[self tableView] deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
     else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
 /*
 // Override to support rearranging the table view.
