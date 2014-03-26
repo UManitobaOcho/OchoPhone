77b6d84d7b771db14aeedd0bbaebd39898fbf1e9
Hugo <cabralth@myumanitoba.ca>
2014-03-25 19:55:51 -0500

    some fixes

--- OchoPad/OchoPad/AssignmentTableViewController.m
+++ OchoPad/OchoPad/AssignmentTableViewController.m
@@ -49,21 +49,6 @@
     [mySocketIO sendEvent:@"getAssignmentsForCourse" withData:data];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    [ComInterface sharedInstance].delegate = self;
-    SocketIO *mySocketIO = [ComInterface sharedInstance].socketIO;
-
-    assignments = [[NSMutableArray alloc] init];
-    NSDictionary *data = [NSDictionary dictionaryWithObjectsAndKeys:
-                          @1, @"course_id", nil];
-    
-    [mySocketIO sendEvent:@"getAssignmentsForCourse" withData:data];
-
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -163,73 +148,5 @@
         */
     }
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSIndexPath *indexP;
-    
-    while([assignments count] > 0) {
-        indexP = [NSIndexPath indexPathForRow:([assignments count] - 1) inSection:0];
-        [assignments removeObjectAtIndex:0];
-        [[self tableView] deleteRowsAtIndexPaths:@[indexP] withRowAnimation:UITableViewRowAnimationFade];
-    }
-    
-    for (int i = 0; i < rowCount; i++) {
-        NSLog(@"received response >>> data: %@", response[i]);
-        
-        ProfAssignment *assignment = [[ProfAssignment alloc] init];
-        assignment.AssignmentName = response[i][@"assignment_name"];
-        assignment.DueDate = response[i][@"due_date"];
-        assignment.ReleaseDate = response[i][@"viewable_date"];
-        [assignments addObject:assignment];
-        
-        
-        NSLog(@"Assignment name: %@", assignment.AssignmentName);
-        NSLog(@"received assignment >>> data: %@", assignment);
-        NSLog(@"received response >>> data: %d", [assignments count]);
-        
-        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([assignments count] - 1) inSection:0];
-        [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-        
-    }
-}
-
-- (void)receivedPacket:(id)packet
-{
-    NSLog(@"receivedPacketAssigments >>>>> ");
-    NSLog(@"got Packet:\n%@", packet);
-    
-    NSArray *response = packet[@"args"][0];
-    NSLog(@"\ngot Response:\n%@", response);
-    //NSInteger count = [(NSNumber *)[packet[@"args"][0] objectForKey:@"rowCount"] integerValue];
-    NSInteger count = [response count];
-    
-    NSLog(@"Response count:\n%d", count);
-    
-    if([packet[@"name"] isEqual: @"foundAssignments"])
-    {
-        [self fillAssignmentList:response rowCount:count];
-    }
-    
-    //else if([packet[@"name"] isEqual: @"ProfAssignmentSubmitted"])
-    //{
-    //  NSLog(@"Professor Assignment Added Successfully");
-    //[self dismissViewControllerAnimated:YES completion:nil];
-    //[self.navigationController popViewControllerAnimated:YES];
-    //}
-}
-
 
 @end
